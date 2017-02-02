d84cf8badb0a895765c7c316cbd944c1c95726d8
Ethan Passmore <ethan.passmore@gmail.com>
2017-02-01 23:19:34 -0500

    Implemented Maddy's code into Drivetrain, including renaming stuff (1/2 done)

--- src/org/usfirst/frc/team1895/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team1895/robot/subsystems/Drivetrain.java
@@ -1,10 +1,16 @@
 package org.usfirst.frc.team1895.robot.subsystems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.usfirst.frc.team1895.robot.RobotMap;
 import org.usfirst.frc.team1895.robot.commands.drivetrain.DefaultDrive;
 
 import com.ctre.CANTalon;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -29,24 +35,48 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Drivetrain extends Subsystem {
 
-	//Instance variables. There should only be one instance of Drivetrain, but we are
-	//assuming the programmer will not accidently create multiple instances
-	private CANTalon  leftMotor1;
-	private CANTalon  leftMotor2;
-	private CANTalon  leftMotor3;
-	private CANTalon rightMotor1;
-	private CANTalon rightMotor2;
-	private CANTalon rightMotor3;
+	// Instance variables. There should only be one instance of Drivetrain, but we are
+	// assuming the programmer will not accidently create multiple instances
 	
+	// Create CANTalons here so we can access them in the future, if wee need to
+	private CANTalon fuelside_motor1;
+	private CANTalon fuelside_motor2;
+	private CANTalon fuelside_motor3;
+	private CANTalon gearside_motor1;
+	private CANTalon gearside_motor2;
+	private CANTalon gearside_motor3;
+	
+	// Motorgroups
+	private MotorGroup<CANTalon> fuelside_motorgroup;
+	private MotorGroup<CANTalon> gearside_motorgroup;
+	
+	// Digital sensors
+	private Encoder left_gearbox_encoder;
+	private Encoder right_gearbox_encoder;
+	
+	// Analog sensors
+	private AnalogGyro  gyro;
+	private AnalogInput rangefinder;
+	
+	// Instanciate all of the variables, and add the motors to their respective
+	// MotorGroup.
 	public Drivetrain() {
 		
-		//instanciate the instance variables
-		 leftMotor1 = new CANTalon(RobotMap.LEFT_MOTOR_1_PORT);
-		 leftMotor2 = new CANTalon(RobotMap.LEFT_MOTOR_2_PORT);
-		 leftMotor3 = new CANTalon(RobotMap.LEFT_MOTOR_3_PORT);
-		rightMotor1 = new CANTalon(RobotMap.RIGHT_MOTOR_1_PORT);
-		rightMotor2 = new CANTalon(RobotMap.RIGHT_MOTOR_2_PORT);
-		rightMotor3 = new CANTalon(RobotMap.RIGHT_MOTOR_3_PORT);
+		fuelside_motor1 = new CANTalon(RobotMap.LEFT_MOTOR_1_PORT);
+		fuelside_motor2 = new CANTalon(RobotMap.LEFT_MOTOR_2_PORT);
+		fuelside_motor3 = new CANTalon(RobotMap.LEFT_MOTOR_3_PORT);
+		gearside_motor1 = new CANTalon(RobotMap.RIGHT_MOTOR_1_PORT);
+		gearside_motor2 = new CANTalon(RobotMap.RIGHT_MOTOR_2_PORT);
+		gearside_motor3 = new CANTalon(RobotMap.RIGHT_MOTOR_3_PORT);
+		
+		fuelside_motorgroup = new MotorGroup<CANTalon>(fuelside_motor1, fuelside_motor2, fuelside_motor3);
+    	gearside_motorgroup = new MotorGroup<CANTalon>(gearside_motor1, gearside_motor2, gearside_motor3);
+    	
+    	fuelside_encoder = new Encoder(RobotMap.FS_GEARBOX_ENCODER_A_PORT, RobotMap.FS_GEARBOX_ENCODER_B_PORT);
+    	gearside_encoder = new Encoder(RobotMap.GS_GEARBOX_ENCODER_A_PORT, RobotMap.GS_GEARBOX_ENCODER_B_PORT);
+    	
+    	rangefinder = new AnalogInput(RobotMap.RANGEFINDER_PORT);
+    	gyro = new AnalogGyro(RobotMap.GYRO_PORT);
 		
 		//TODO: Add the motors created above into MotorGroups, once MotorGroup is implemented.
 	}
@@ -55,18 +85,18 @@ public class Drivetrain extends Subsystem {
     // here. Call these from Commands.
 	
 	
-	//A basic tank drive method. The two parameters are expected to be within the range -1.0 to 1.0
-	//If not, they are limited to be within that range (to be added after MVP) The parameters
-	//will set their respective side of robot to the given value.
-	public void tankDrive(double left, double right) {
-		//TODO: Limit the parameters to be within range [-1.0, 1.0]
-		//TODO: replace individual motor.set() calls to that side's respective MotorGoup .set() method
-		 leftMotor1.set( left);
-		 leftMotor2.set( left);
-		 leftMotor3.set( left);
-		rightMotor1.set(right);
-		rightMotor2.set(right);
-		rightMotor3.set(right);
+	// A basic tank drive method. The two parameters are expected to be within the range -1.0 to 1.0
+	// If not, they are limited to be within that range (to be added after MVP) The parameters
+	// will set their respective side of robot to the given value.
+	public void tankDrive(double fuelside, double gearside) {
+		// Motors need to be in the range [-1.0, 1.0], so check to see if they are NOT
+		// If they aren't, then limit the value to be the maximum value.
+		if(fuelside >  1.0) fuelside =  1.0;
+		if(fuelside < -1.0) fuelside = -1.0;
+		if(gearside >  1.0) gearside =  1.0;
+		if(gearside < -1.0) gearside = -1.0;
+		fuelside_motorgroup.set(fuelside);
+		gearside_motorgroup.set(gearside);
 	}
 	
     public void initDefaultCommand() {
@@ -77,9 +107,22 @@ public class Drivetrain extends Subsystem {
     
     /**Thar be dragons when motors on the same gearbox are set differently, so a MotorGroup will handle setting multiple
      * motors to the same value as if it were one motor.*/
-    @SuppressWarnings("unused")
-	private class MotorGroup<T extends SpeedController> {
-    	//TODO: Have Maddy finish implementing MotorGroup
+    private class MotorGroup<T extends SpeedController> {
+    	//list of type T
+    	private List<T> list = new ArrayList<T>();
+    	
+    	//constructor
+    	private MotorGroup(T...t) {	//so you can add as many motors of type T as needed per gearbox
+    		for(T i : t) {			//for the number of motors, to every motor
+    			list.add(i);		//add a motor to the arraylist
+    		}
+    	}
+    	
+    	private void set(double v) { //sets velocity of each motorgroup
+			for(T i : list) {
+				i.set(v);			 //ensures they all go the same speed
+			}
+		}
     }
 }
 
