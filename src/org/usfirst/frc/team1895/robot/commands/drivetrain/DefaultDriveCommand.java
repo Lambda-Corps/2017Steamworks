db860e9b9132450ef03a6c9d315c77a633ce4e77
mseputro1 <mseputro1220@gmail.com>
2017-02-11 10:55:09 -0500

    added untested arcade drive, I'm only pushing so I can move computers

--- src/org/usfirst/frc/team1895/robot/commands/drivetrain/DefaultDriveCommand.java
+++ src/org/usfirst/frc/team1895/robot/commands/drivetrain/DefaultDriveCommand.java
@@ -37,7 +37,7 @@ public class DefaultDriveCommand extends Command {
 		// Joystick.getY() returns the Z axis, so use getRawAxis(1) (the y axis)
 		// until the custom classes are implemented.
 		//TODO: Implement Arcade Joystick or F310 get axis methods
-		//Robot.drivetrain.arcadeDrive(Robot.oi.gamepad.getAxis(F310.LX), Robot.oi.gamepad.getAxis(F310.LY) );
+		Robot.drivetrain.arcadeDrive(Robot.oi.gamepad.getAxis(F310.LY), Robot.oi.gamepad.getAxis(F310.RX));
 		Robot.drivetrain.tankDrive(-Robot.oi.leftArcadeJoystick.getRawAxis(1)*0.75, Robot.oi.rightArcadeJoystick.getRawAxis(1)*0.75);
 	}
 
