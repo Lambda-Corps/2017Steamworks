4b6fa62278df73c854f2b7099ac353f883e12307
mseputro1 <mseputro1220@gmail.com>
2017-03-04 08:31:27 -0500

    adding three more autonomouses + a few changes in route, so that in the end if we get the gear out we will drive to the neutral zone and end in front of the opponent's boiler

---
+++ src/org/usfirst/frc/team1895/robot/commands/autonomous/BRight_Position3_Autonomous.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team1895.robot.commands.autonomous;
+
+import org.usfirst.frc.team1895.robot.commands.drivetrain.DriveStraightSetDistance;
+import org.usfirst.frc.team1895.robot.commands.drivetrain.DriveToObstacle;
+import org.usfirst.frc.team1895.robot.commands.drivetrain.TurnWithGyro;
+import org.usfirst.frc.team1895.robot.commands.gears.DeployGearHolder;
+import org.usfirst.frc.team1895.robot.commands.gears.RetractGearHolder;
+import org.usfirst.frc.team1895.robot.commands.gears.WaitUntilGearGoneOrTimeOut;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class BRight_Position3_Autonomous extends CommandGroup {
+
+    public BRight_Position3_Autonomous() {
+    	
+    	addSequential(new DriveStraightSetDistance(-83)); //117
+    	//TURN TOWARD TO AIRSHIP'S LIFT
+    	addSequential(new TurnWithGyro(60)); //this angle depends on where we are on the field
+    	//DEPLOY GEARHOLDER [DRIVE UP A LITTLE IF NEEDED, OR ELSE USE DRIVETOOBSTACLE, DEPENDS ON DISTANCE
+    	addParallel(new DeployGearHolder());
+    	//31
+    	addSequential(new DriveStraightSetDistance(-3)); //driving the hypotenuse
+    	//ALIGN TO LIFT
+    	addSequential(new DriveToObstacle(16, 0.15)); 	//
+    	//addSequential(new AlignToPeg());
+    	//WAIT TILL GEAR IS GONE
+    	addSequential(new WaitUntilGearGoneOrTimeOut(4));
+    	
+    	//RETRACT GEAR HOLDER AND DRIVE BACK
+    	addParallel(new RetractGearHolder());
+    	addSequential(new DriveStraightSetDistance(31));
+    	//TURN SO FACING FORWARD AGAIN
+    	addSequential(new TurnWithGyro(-60.0));
+    	//DRIVE FORWARD INTO NEUTRAL ZONE
+    	addSequential(new DriveStraightSetDistance(-40));	
+    	addSequential(new TurnWithGyro(60.0));		//Mr.Brey wants us to drive toward the boiler but stay in NZ
+    	addSequential(new DriveStraightSetDistance(-50));
+    }
+}
