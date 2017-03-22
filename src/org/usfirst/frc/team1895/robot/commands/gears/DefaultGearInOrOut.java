12c50944fb0119421a3bd81b99eb3441b98aa2ca
1895 <mseputro1220@gmail.com>
2017-03-22 19:22:51 -0400

    ethan's gear in or out sensor

--- src/org/usfirst/frc/team1895/robot/commands/gears/DefaultGearInOrOut.java
+++ src/org/usfirst/frc/team1895/robot/commands/gears/DefaultGearInOrOut.java
@@ -21,7 +21,7 @@ public class DefaultGearInOrOut extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	SmartDashboard.putBoolean("Is the gearholder out?", Robot.gearholder.getGearState());
+    	//SmartDashboard.putBoolean("Is the gearholder out?", Robot.gearholder.getGearState());
     }
 
     // Make this return true when this Command no longer needs to run execute()
